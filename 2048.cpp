bd69de39ebb54be7a53edbb586fc8cbbbcd13819
shahom1084 <omshah429@gmail.com>
2025-04-19 02:22:01 +0530

    Include random tile genration without testing

--- 2048.cpp
+++ 2048.cpp
@@ -1,8 +1,26 @@
 #include<iostream>
-int grid[4][4]={ 0 };
-// int random_tile(int grid){
+#include<cstdlib>
+#include<ctime>
+using namespace std;
 
-// }
+int grid[4][4]={ 0 };
+int random_tile(int grid[][]){
+    srand(time(nullptr));
+    int x=rand()%4;
+    srand(time(nullptr));
+    int y=rand()%4;
+    srand(time(nullptr));
+    int num=rand()%2;
+    if(num%2==0){
+        grid[x][y]=2;
+    }
+    else{
+        grid[x][y]=4;
+    }
+
+
+
+}
 int main() {
     return 0;
 }
