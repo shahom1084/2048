af0e8123a7318c125178ee062ef52bcc100e344b
shahom1084 <omshah429@gmail.com>
2025-04-20 01:40:33 +0530

    full game done with testing

--- 2048.cpp
+++ 2048.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 int grid[4][4]={ 0 };
+int score=0;
+int best_score=0;
 void random_tile(){
     int x=rand()%4;
     int y=rand()%4;
@@ -32,6 +34,7 @@ void print_grid(int grid[][4],int x,int y){
         
     
     }
+    cout<<"Score: "<<score<<"\n";
 }
 void start_screen(int grid[4][4]){
     cout<<"Welcome to 2048 Game\n";
@@ -46,6 +49,7 @@ void start_screen(int grid[4][4]){
         random_tile();
         random_tile();
         print_grid(grid,4,4);
+        cout<<"Best Score "<< best_score<<"\n";
     }
     else{
         exit(0);
@@ -66,6 +70,7 @@ void move_left(){
                         if(grid[i][k]==grid[i][k+1]){
                             grid[i][k]=grid[i][k+1]*2;
                             grid[i][k+1]=0;
+                            score+=grid[i][k];
                         }
                         else{
                             break;
@@ -80,8 +85,136 @@ void move_left(){
     }
     
 }
+
+void move_right(){
+    for(int i;i<4;i++){
+        for(int j=0;j<4;j++){
+            if(grid[i][j]!=0){
+                int k=j+1;
+                while(k<=3){
+                    if(grid[i][k]==0){
+                        grid[i][k]=grid[i][k-1];
+                        grid[i][k-1]=0;
+                    }
+                    else{
+                        if(grid[i][k]==grid[i][k-1]){
+                            grid[i][k]=grid[i][k-1]*2;
+                            grid[i][k-1]=0;
+                            score+=grid[i][k];
+                        }
+                        else{
+                            break;
+                        }
+                    }
+                    k++;
+
+                }
+
+            }
+        }
+    }
+    
+}
+
+void move_up(){
+    for(int i;i<4;i++){
+        for(int j=3;j>=0;j--){
+            if(grid[i][j]!=0){
+                int k=i-1;
+                while(k>=0){
+                    if(grid[k][j]==0){
+                        grid[k][j]=grid[k+1][j];
+                        grid[k+1][j]=0;
+                    }
+                    else{
+                        if(grid[k][j]==grid[k+1][j]){
+                            grid[k][j]=grid[k+1][j]*2;
+                            grid[k+1][j]=0;
+                            score+=grid[i][k];
+                        }
+                        else{
+                            break;
+                        }
+                    }
+                    k--;
+
+                }
+
+            }
+        }
+    }
+    
+}
+
+void move_down(){
+    for(int i;i<4;i++){
+        for(int j=0;j<4;j++){
+            if(grid[i][j]!=0){
+                int k=i+1;
+                while(k<=3){
+                    if(grid[k][j]==0){
+                        grid[k][j]=grid[k-1][j];
+                        grid[k-1][j]=0;
+                    }
+                    else{
+                        if(grid[k][j]==grid[k-1][j]){
+                            grid[k][j]=grid[k-1][j]*2;
+                            grid[k-1][j]=0;
+                            score+=grid[i][k];
+                        }
+                        else{
+                            break;
+                        }
+                    }
+                    k++;
+
+                }
+
+            }
+        }
+    }
+    
+}
+void update_best_score(){
+    if(score>best_score){
+        best_score=score;
+        
+    }
+}
+bool game_over(){
+    
+    int traversal_counter=0;
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            
+            if(grid[i][j]==0 || grid[i][j]==grid[i][j-1] || grid[i][j]==grid[i][j+1] || grid[i][j]==grid[i-1][j] || grid[i][j]==grid[i+1][j]){
+                traversal_counter++;
+                return false;
+            }
+            
+            
+            
+        }
+        
+    }
+    if(traversal_counter==0){
+        return true;
+    }
+}
+
+bool win_game(){
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            if(grid[i][j]==2048){
+                return true;
+            }
+        }
+    }
+    return false;
+}
 int gameplay(int grid[][4]){
     bool game=true;
+
     while(game){
         cout<<"1. -> \n";
         cout<<"2. <- \n";
@@ -92,28 +225,44 @@ int gameplay(int grid[][4]){
         int choice;
         cin>>choice;
         if(choice==1){
-            // move_right(grid);
+            move_right();
         }
         else if(choice==2){
             move_left();
         }
         else if(choice==3){
-            // move_up(grid);
+            move_up();
         }
         else if(choice==4){
-            // move_down(grid);
+            move_down();
 
         }
         else if(choice==5){
+            cout<<"Thank you!Play again!"<<"\n";
+            cout<<"Score"<<score<<"\n";
+            cout<<"Best Score"<<best_score<<"\n";
             game=false;
+            
             break;
         }
         else{
+
             cout<<"Uh oh!";
             continue;
         }
         random_tile();
         print_grid(grid,4,4);
+        update_best_score();
+        if(game_over()){
+            cout<<"Game Over!Try Again\n";
+            cout<<"Score"<<score<<"\n";
+            cout<<"Best Score"<<best_score<<"\n";
+        }
+        // if(win_game()){
+        //     cout<<"COngratulations! You Win!\n";
+        //     cout<<"Score"<<score<<"\n";
+        //     cout<<"Best Score"<<best_score;
+        // }
     }
 }
 
